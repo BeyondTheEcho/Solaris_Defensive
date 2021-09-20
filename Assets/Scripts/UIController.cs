4244893758e157c04dee19fbafd51b94f08cb042
Clare_Chan1738 <claregpbenn@icloud.com>
2021-09-20 18:09:22 -0400

    Added Options Menu & Features
    - Added Options Menu and working buttons
    - Options button links to Options Canvas
    - Main Menu button links to Main Menu Canvas
    - Exit button closes game player & game application

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class UIController : MonoBehaviour
 {
+    //Config
+    [SerializeField] GameObject OptionsMenu;
+    [SerializeField] GameObject MainMenu;
+
+    bool MenuSwitch = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +21,7 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        OptionsMenuChecker();
     }
 
     //This Function Starts The Game When Called As An On Click Function
@@ -24,4 +31,35 @@ public class UIController : MonoBehaviour
         SceneManager.LoadScene("Level 1");
     }
 
+    public void ExitGame()
+    {
+        //Exits Game
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    public void OptionsMenuChecker()
+    {
+            if (MenuSwitch == true)
+            {
+                OptionsMenu.gameObject.SetActive(true);
+                MainMenu.gameObject.SetActive(false);
+            }
+            else if (MenuSwitch == false)
+            {
+                OptionsMenu.gameObject.SetActive(false);
+                MainMenu.gameObject.SetActive(true);
+            }
+    }
+    public void MenuSwitcher()
+    {
+        if (MenuSwitch == true)
+        {
+            MenuSwitch = false;
+        }
+        else if (MenuSwitch == false)
+        {
+            MenuSwitch = true;
+        }
+    }
 }
