487723e03477f08ea83b210bf212361e764eab2f
Stelios Klapsis <Steliosklapsis@outlook.com>
2021-09-27 02:20:56 -0400

    Created Wave Config Scriptable Object
    - Created a wave config scriptable object in order to aid in automating the enemy pathing process.
    - When this system is fleshed out it will allow for much faster level design.

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;  
     [SerializeField] float moveSpeed = 2f;
+
+    List<Transform> waypoints;
 
     int currentWaypoint = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[currentWaypoint].transform.position;       
     }
 
